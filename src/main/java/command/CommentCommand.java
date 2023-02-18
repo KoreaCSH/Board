eb9edeb92bafb0c8fe04eef72fbe7c18003936d7
csh <tmdgjs7495@naver.com>
2023-02-18 10:05:48 +0900

    18th commit - CommentController

---
+++ src/main/java/command/CommentCommand.java
@@ -0,0 +1,15 @@
+package command;
+
+public class CommentCommand {
+
+	private String comment_content;
+
+	public String getContent() {
+		return comment_content;
+	}
+
+	public void setContent(String comment_content) {
+		this.comment_content= comment_content;
+	}
+	
+}
